9da208bdd851067c467b7c0100a6f312a04dd69f
yunju <dbswnfnal@naver.com>
2024-07-11 11:00:01 +0900

    240711 react hello app 생성

---
+++ React/hello/src/1.Hello.js
@@ -0,0 +1,7 @@
+function Hello() {
+    return (
+        <h1>Hello from Hello.js!</h1>
+    );
+}
+
+export default Hello;
---
+++ React/hello/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Hello from "./1.Hello.js";
+
+function App() {
+  return (
+    <Hello />
+  );
+}
+
+export default App;
